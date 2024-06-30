f78594be04f2ffa3477d2ddaf7978fe991c945d4
naveen k <naveennavi1711@gmail.com>
2024-06-30 10:20:48 +0530

    Update: describe your changes here

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,34 @@
 import {Bars3Icon } from '@heroicons/react/24/solid'
 import { useState } from 'react'
-export default function Header()
-{
-    const[toggleMenu,setToggleMenu]=useState(false)
-    return <header className="flex justify-between px-5 py-3 bg-primary font-Home-font">
-        <a className='font-bold text-black font-Home-font' href="#">NAVEEN.K</a>
-        <nav className='hidden md:block'>
-        <ul className="flex">
-            <li><a href="/">HOME</a></li>
-            <li><a href="#about">ABOUT ME</a></li>
-            <li><a href="#project">PROJECTS</a></li>
-            <li><a href="#resume">RESUME</a></li>
-            <li><a href="#contact">CONTACT</a></li>
-            
-        </ul>
-        </nav>
-        { toggleMenu &&
-            <nav className='block md:hidden '>
-            <ul onClick={()=>setToggleMenu(!toggleMenu)} className="flex flex-col mobile-nav">
-                <li><a href="/">HOME</a></li>
-                <li><a href="#about">ABOUT ME</a></li>
-                <li><a href="#project">PROJECTS</a></li>
-                <li><a href="#resume">RESUME</a></li>
-                <li><a href="# contact">CONTACT</a></li>
-            </ul>
+export default function Header() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    return (
+        <header className="flex justify-between px-5 py-3 bg-primary font-Home-font">
+            <a className='font-bold text-black font-Home-font' href="/" aria-label='Home page'>NAVEEN.K</a>
+            <nav className='hidden md:block'>
+                <ul className="flex">
+                    <li><a href="/">HOME</a></li>
+                    <li><a href="#about">ABOUT ME</a></li>
+                    <li><a href="#project">PROJECTS</a></li>
+                    <li><a href="#resume">RESUME</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
+                </ul>
             </nav>
-        }
-        <button className='block md:hidden' onClick={()=>setToggleMenu(!toggleMenu)}><Bars3Icon className='text-black h-6'/></button>
-        
-    </header>
+            {toggleMenu &&
+                <nav className='block md:hidden'>
+                    <ul onClick={() => setToggleMenu(!toggleMenu)} className="flex flex-col mobile-nav">
+                        <li><a href="/">HOME</a></li>
+                        <li><a href="#about">ABOUT ME</a></li>
+                        <li><a href="#project">PROJECTS</a></li>
+                        <li><a href="#resume">RESUME</a></li>
+                        <li><a href="#contact">CONTACT</a></li>
+                    </ul>
+                </nav>
+            }
+            <button className='block md:hidden' onClick={() => setToggleMenu(!toggleMenu)}>
+                <Bars3Icon className='text-black h-6' />
+            </button>
+        </header>
+    );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -17,24 +17,26 @@ export default function Project() {
   };
 
   return (
-    <section id='project' className='w-full  font-Home-font'>
+    <section id='project' className='w-full font-Home-font'>
       <div className='flex flex-col pt-16'>
         <h1 className='text-4xl mb-5 text-center'>
           <span className='border-b-4 border-[#318445]'>PROJECT</span>
         </h1>
       </div>
       <div className='flex w-full px-6 py-5 flex-wrap md:flex-nowrap'>
-        <div className='w-full md:w-1/2 '>
+        <div className='w-full md:w-1/2'>
           <div className='flex flex-col items-center'>
             <h1 className='text-4xl mb-5 px-6 font-bold border-b-4 border-[#318445] w-46 text-center'>
               Description
             </h1>
           </div>
-          <p className='py-3 px-6 text-justify' >Parkinson's disease is a complex condition with multiple risk factors, making early diagnosis challenging. Our project aims to improve the accuracy of predicting Parkinson's disease using machine learning techniques. By identifying effective models and selecting relevant features, we achieved an evaluation accuracy of 98% on 195 samples. Early diagnosis is crucial for better patient outcomes, and our research aims to enhance predictive capabilities. Future work will focus on hybridization to further increase accuracy and decrease error rates.</p>
+          <p className='py-3 px-6 text-justify'>
+            Parkinson's disease is a complex condition with multiple risk factors, making early diagnosis challenging. Our project aims to improve the accuracy of predicting Parkinson's disease using machine learning techniques. By identifying effective models and selecting relevant features, we achieved an evaluation accuracy of 98% on 195 samples. Early diagnosis is crucial for better patient outcomes, and our research aims to enhance predictive capabilities. Future work will focus on hybridization to further increase accuracy and decrease error rates.
+          </p>
         </div>
         <div className='w-full md:w-1/2 flex justify-center'>
           <div className='max-w-[600px] h-[300px] w-full m-auto py-8 px-8 relative'>
-            <img className='w-full h-full rounded-2xl bg-center duration-500' src={slides[currentIndex]} alt={`Image ${currentIndex + 1}`} />
+            <img className='w-full h-full rounded-2xl bg-center duration-500' src={slides[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
             <div className='absolute top-1/2 left-5 transform -translate-y-1/2 text-2xl rounded-full p-2 bg-black text-white cursor-pointer'>
               <FaArrowLeft onClick={handlePrev} />
             </div>
